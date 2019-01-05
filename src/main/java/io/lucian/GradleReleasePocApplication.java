e0cabb4e4516b0a0f64c40c1e0429ac992f4acf4
Lucian Machado <lucian.m.oliveira@gmail.com>
2019-01-05 03:20:03 -0200

    Criação do projeto.

---
+++ src/main/java/io/lucian/GradleReleasePocApplication.java
@@ -0,0 +1,14 @@
+package io.lucian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleReleasePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleReleasePocApplication.class, args);
+    }
+
+}
+
